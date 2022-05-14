8d33d08b2d3876348287be7f32fa1a0c6b90adcb
Juan Pablo Dìaz Correa <juanipis@gmail.com>
2022-05-14 12:07:17 -0500

    Diccionario --> Frecuencias de caracteres
    Nombramos mal la clase diccionario que teniamos, así que le cambiamos el nombre, la clase diccionario debe ser la que contenga los codigos en binario

--- Huffman/src/huffman/Main.java
+++ Huffman/src/huffman/Main.java
@@ -5,16 +5,16 @@ import java.util.PriorityQueue;
 public class Main {
 
 	public static void main(String[] args) {
-		PriorityQueue<Diccionario> colaPrueba = new PriorityQueue<>();
-		Diccionario d1 = new Diccionario('A',3);
-		Diccionario d2 = new Diccionario('B',4);
-		Diccionario d3 = new Diccionario('C',2);
-		Diccionario d4 = new Diccionario('D',1);
+		PriorityQueue<FrecuenciasCaracteres> colaPrueba = new PriorityQueue<>();
+		FrecuenciasCaracteres d1 = new FrecuenciasCaracteres('A',3);
+		FrecuenciasCaracteres d2 = new FrecuenciasCaracteres('B',4);
+		FrecuenciasCaracteres d3 = new FrecuenciasCaracteres('C',2);
+		FrecuenciasCaracteres d4 = new FrecuenciasCaracteres('D',1);
 		colaPrueba.add(d1);
 		colaPrueba.add(d2);
 		colaPrueba.add(d3);
 		colaPrueba.add(d4);
-		Nodo<Diccionario> nodoPruebaNodo = CreadorArbol.construccion(colaPrueba);
+		Nodo<FrecuenciasCaracteres> nodoPruebaNodo = CreadorArbol.construccion(colaPrueba);
 		System.out.println(nodoPruebaNodo.getHijoDerecha());
 
 		// Test metodo reconocimientoCaracteres
